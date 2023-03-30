78cf1ddc773f0c79ccac3d812eef61f5ef7d803d
galifornia <martinho87@gmail.com>
2023-03-31 00:59:24 +0200

    fix: typescript not detecting that users with no username have been filtered

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -4,22 +4,24 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 
-import { Post } from "../../../../node_modules/.prisma/client/index.d";
 import { TRPCError } from "@trpc/server";
+import { Post } from "@prisma/client";
 
 const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profilePictureUrl: user.profileImageUrl,
-  };
+  if (user.username) {
+    return {
+      id: user.id,
+      username: user.username,
+      profilePictureUrl: user.profileImageUrl,
+    };
+  }
 };
 
 export const postsRouter = createTRPCRouter({
   getAllPosts: publicProcedure
     .input(z.string())
     .query(async ({ input, ctx }) => {
-      const posts = await ctx.prisma.post.findMany({
+      const posts: Post[] = await ctx.prisma.post.findMany({
         where: { userId: input },
         take: 100,
       });
@@ -31,11 +33,13 @@ export const postsRouter = createTRPCRouter({
           }) as string[],
           limit: 100,
         })
-      ).map(filterUserForClient);
+      )
+        .map(filterUserForClient)
+        .filter(Boolean);
 
       return posts.map((post: Post) => {
         const author = users.find((user) => user.id === post.userId);
-        if (!author || !author.username)
+        if (!author)
           throw new TRPCError({
             code: "INTERNAL_SERVER_ERROR",
             message: "Author for post not found",
