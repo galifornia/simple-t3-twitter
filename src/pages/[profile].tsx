8882716f1692542e625979566933387155bab878
galifornia <martinho87@gmail.com>
2023-04-05 12:32:48 +0100

    feat: /[profile] trpc ssg helpers to prefetch user

--- src/pages/[profile].tsx
+++ src/pages/[profile].tsx
@@ -1,27 +1,57 @@
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+} from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import LoadingSpinner from "~/components/LoadingSpinner";
+import superson from "superjson";
+import { appRouter } from "~/server/api/root";
 import { api } from "~/utils/api";
 
-const ProfilePage = () => {
-  const router = useRouter();
-  const { profile } = router.query;
-  const { data, isLoading } = api.profiles.getUserByUsername.useQuery(
-    profile as string
-  );
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 
-  if (isLoading) return <LoadingSpinner size={64} />;
+import { prisma } from "../server/db";
+
+const ProfilePage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { profile } = props;
+  const { data } = api.profiles.getUserByUsername.useQuery(profile as string);
 
   if (!data) return <div>404</div>;
 
   return (
     <>
       <Head>
-        <title>Chirp | Profile</title>
+        <title>Chirp | {profile}</title>
       </Head>
       <div>Profile of @{data.username}</div>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: "" },
+    transformer: superson,
+  });
+
+  const profile = context.params?.profile as string;
+
+  await ssg.profiles.getUserByUsername.prefetch(profile);
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      profile,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 };
 
 export default ProfilePage;
