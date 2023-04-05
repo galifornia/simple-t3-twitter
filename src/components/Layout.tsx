fcbdc8d03c63e6d44b39cab673febbf194bbd7cd
galifornia <martinho87@gmail.com>
2023-04-05 23:16:02 +0100

    feat: profile layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="mx-auto flex max-h-screen w-full max-w-3xl flex-col overflow-hidden border-x border-slate-400">
+    <main className="mx-auto flex max-h-screen w-full max-w-3xl flex-col border-x border-slate-400">
       {children}
     </main>
   );
--- src/pages/[profile].tsx
+++ src/pages/[profile].tsx
@@ -4,6 +4,7 @@ import {
   InferGetStaticPropsType,
 } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import superson from "superjson";
 import Layout from "~/components/Layout";
 import { appRouter } from "~/server/api/root";
@@ -25,7 +26,20 @@ const ProfilePage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
         <title>Chirp | {profile}</title>
       </Head>
       <Layout>
-        <div>Profile of @{data.username}</div>
+        <div className="relative flex h-24 items-center gap-8 border-b border-slate-400 p-4">
+          <Image
+            className="border-slate-9000 absolute -bottom-0 left-0 -mb-12 ml-4 rounded-full border-2 border-black"
+            width={96}
+            height={96}
+            src={data.profilePictureUrl}
+            alt={`profile picture of @${data.username}`}
+          />
+        </div>
+        <div className="border-b border-slate-400 p-4">
+          <div className="h-16"></div>
+          <div className="text-2xl font-bold">@{data.username}</div>
+        </div>
+        <div className="h-screen"></div>
       </Layout>
     </>
   );
