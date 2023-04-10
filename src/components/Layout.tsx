6ea8883e544cca94255b9a1804945c9769f5c48f
galifornia <martinho87@gmail.com>
2023-04-10 11:22:35 +0100

    fix: Layout top padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="mx-auto flex w-full max-w-3xl flex-col px-4 lg:px-0">
+    <main className="mx-auto flex w-full max-w-3xl flex-col px-4 pt-8 lg:px-0">
       {children}
     </main>
   );
