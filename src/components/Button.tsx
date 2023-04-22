c3f3fcd5e131b7bc1888a28736ea812191134d17
galifornia <martinho87@gmail.com>
2023-04-22 22:46:39 +0200

    feat: Button - primary change on hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-slate-900 text-primary-foreground hover:bg-slate-700 hover:border-slate-900 border border-transparent",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
