9b10a23733c58edb9e5bec5aa4cab1e481bd809c
galifornia <martinho87@gmail.com>
2023-04-23 23:52:45 +0200

    fix: PostSkeleton should have rounded banner

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -14,7 +14,7 @@ function Skeleton({
 
 export const PostSkeleton = () => (
   <div className="my-4 flex w-full items-center gap-4">
-    <Skeleton className="h-14 w-14 rounded-full" />
+    <Skeleton className="aspect-square h-14 w-14 rounded-full" />
     <div className="w-full space-y-2">
       <Skeleton className="h-10 w-[90%]" />
       <Skeleton className="h-10 w-[80%]" />
