c864542d794c2d62d4b40b92def00ab696f1a403
galifornia <martinho87@gmail.com>
2023-04-24 21:03:30 +0200

    feat: change address bar background color in iOS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -53,6 +53,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </Head>
       <Toaster />
 
